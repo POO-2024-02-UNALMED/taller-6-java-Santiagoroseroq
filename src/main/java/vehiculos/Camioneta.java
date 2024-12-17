49f55778273dd46040cf1efb8698712b21cde191
Santiagoroseroq <sroseroq@unal.edu.co>
2024-12-17 13:48:57 -0500

    Update Camioneta.java

--- src/main/java/vehiculos/Camioneta.java
+++ src/main/java/vehiculos/Camioneta.java
@@ -1,21 +1,21 @@
 package vehiculos;
 
 public class Camioneta extends Vehiculo {
-    private boolean volco;
+    private boolean cuatroXCuatro;  // Cambié el nombre de la variable para evitar el problema con el "4"
 
-    // Constructor actualizado para coincidir con el test
-    public Camioneta(String placa, int numPuertas, String nombre, int precio, int peso, Fabricante fabricante, boolean volco) {
-        // Llamada al constructor de la clase base Vehiculo con los parámetros adecuados
-        super(placa, numPuertas, peso, nombre, precio, fabricante, "4X4");
-        this.volco = volco;
+    // Constructor
+    public Camioneta(String placa, int puertas, String marca, double precio, double peso, Fabricante fabricante, boolean cuatroXCuatro) {
+        // Llamamos al constructor de Vehiculo
+        super(placa, puertas, 100, marca, precio, peso, "4x4", fabricante);  // "4x4" es un valor predeterminado para la tracción
+        this.cuatroXCuatro = cuatroXCuatro;  // Inicializamos la variable
     }
 
-    // Getter y Setter
-    public boolean isVolco() {
-        return volco;
+    // Getter y Setter para 'cuatroXCuatro'
+    public boolean isCuatroXCuatro() {
+        return cuatroXCuatro;
     }
 
-    public void setVolco(boolean volco) {
-        this.volco = volco;
+    public void setCuatroXCuatro(boolean cuatroXCuatro) {
+        this.cuatroXCuatro = cuatroXCuatro;
     }
 }
